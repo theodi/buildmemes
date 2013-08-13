527279328b5ef3cd11fe764d1e6c4fd83fef9d3d
James Smith <james@floppy.org.uk>
2013-08-13 10:50:22 +0100

    allow jpeg extension in tests

--- spec/redirect_spec.rb
+++ spec/redirect_spec.rb
@@ -15,13 +15,13 @@ describe BuildMemes do
   it "should redirect /p to a pass image" do
     get "/p"
     last_response.should be_redirect
-    last_response.location.should match /\/pass\/[0-9]{2}.[jpg|gif|png]/
+    last_response.location.should match /\/pass\/[0-9]{2}.[jpg|jpeg|gif|png]/
   end
   
   it "should redirect /f to a fail image" do
     get "/f"
     last_response.should be_redirect
-    last_response.location.should match /\/fail\/[0-9]{2}.[jpg|gif|png]/
+    last_response.location.should match /\/fail\/[0-9]{2}.[jpg|jpeg|gif|png]/
   end
 
 end
